0432b870ac9bd1823cd2c7e74a752694997f96e2
Tory Jordan <119288194+HydraRetro@users.noreply.github.com>
2022-12-27 00:41:16 -0800

    Updated README.md and index.html; Added app.js and styles.css

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Simple Score Keeper.
 
-A simple score keeper between two players using html, CSS and JavaScript.
+A simple score keeper between two players using HTML, CSS and JavaScript.
+
+This is my first project using JavaScript so it may be messy but it'll work..I hope. =]
---
+++ app.js
@@ -0,0 +1,59 @@
+const player1Button = document.querySelector('#player1Button');
+const player2Button = document.querySelector('#player2Button');
+const resetButton = document.querySelector('#resetButton');
+const player1Display = document.querySelector('#player1Display');
+const player2Display = document.querySelector('#player2Display');
+const winningScoreSelect = document.querySelector('#playto');
+
+let player1Score = 0;
+let player2Score = 0;
+let winningScore = 3;
+let isGameOver = false;
+
+player1Button.addEventListener('click', function (){
+    if (!isGameOver) {
+        player1Score += 1;
+        if (player1Score === winningScore) {
+            isGameOver = true;
+            player1Display.classList.add('winner');
+            player2Display.classList.add('loser');
+            player1Button.disabled = true;
+            player2Button.disabled = true;
+
+        }
+        player1Display.textContent = player1Score;
+    }
+})
+
+player2Button.addEventListener('click', function (){
+    if (!isGameOver) {
+        player2Score += 1;
+        if (player2Score === winningScore) {
+            isGameOver = true;
+            player2Display.classList.add('winner');
+            player1Display.classList.add('loser');
+            player1Button.disabled = true;
+            player2Button.disabled = true;
+        }
+        player2Display.textContent = player2Score;
+    }
+})
+
+winningScoreSelect.addEventListener('change', function() {
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+resetButton.addEventListener('click', reset)
+
+function reset() {
+    isGameOver = false;
+    player1Score = 0;
+    player2Score = 0;
+    player1Display.textContent = 0;
+    player2Display.textContent = 0;
+    player1Display.classList.remove('winner','loser');
+    player2Display.classList.remove('winner','loser');
+    player1Button.disabled = false;
+    player2Button.disabled = false;
+}
